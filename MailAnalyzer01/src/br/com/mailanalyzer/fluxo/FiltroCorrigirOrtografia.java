1c8f5276dbf27ea1412d9cffd0db7d5013d5a595
fran.herlayne@gmail.com <fran.herlayne@gmail.com@0cc3e97f-d703-2f51-40d7-cab9bb1f6237>
2011-05-07 20:33:57 +0000

    Criação do Filtro Corrigir Ortografia - Fran

---
+++ MailAnalyzer01/src/br/com/mailanalyzer/fluxo/FiltroCorrigirOrtografia.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.mailanalyzer.fluxo;
+
+import org.xeustechnologies.googleapi.spelling.Configuration;
+import org.xeustechnologies.googleapi.spelling.Language;
+import org.xeustechnologies.googleapi.spelling.SpellChecker;
+import org.xeustechnologies.googleapi.spelling.SpellCorrection;
+import org.xeustechnologies.googleapi.spelling.SpellRequest;
+import org.xeustechnologies.googleapi.spelling.SpellResponse;
+
+/**
+ *
+ * @author Herlayne
+ * @contact fran.herlayne@gmail.com
+ * @version 1.0
+ * @Date 07-05-2011
+ * 
+ */
+public class FiltroCorrigirOrtografia {
+    
+  public void CorrigirOrtografia (String args) {
+
+      Configuration config = new Configuration();
+
+        config.setProxy( "my_proxy_host", 8080, "http" );
+         SpellChecker checker = new SpellChecker( );
+         checker.setOverHttps( true ); // Use https (default true from v1.1)
+         checker.setLanguage( Language.PORTUGUESE ); // Use English (default)
+         SpellRequest request = new SpellRequest();
+         request.setText( args );
+         request.setIgnoreDuplicates( true ); // Ignore duplicates
+
+         SpellResponse spellResponse = checker.check( request );
+
+         for( SpellCorrection sc : spellResponse.getCorrections() ){
+             System.out.println("---");
+             for(String s : sc.getWords())
+                 System.out.println(s);
+         }
+             
+  }
+
+ /* public static void main(String []arg){
+      String input = "esti sabadu ta uma merda";
+      FiltroCorrigirOrtografia filtro = new FiltroCorrigirOrtografia();
+      filtro.CorrigirOrtografia(input);
+    }*/
+}
