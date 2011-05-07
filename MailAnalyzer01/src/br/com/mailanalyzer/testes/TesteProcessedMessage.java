cc77badb9ef114554e43bec00e1e9c8b86297558
fran.herlayne@gmail.com <fran.herlayne@gmail.com@0cc3e97f-d703-2f51-40d7-cab9bb1f6237>
2011-05-07 15:48:05 +0000

    Criação do Teste - Fran

---
+++ MailAnalyzer01/src/br/com/mailanalyzer/testes/TesteProcessedMessage.java
@@ -0,0 +1,23 @@
+package br.com.mailanalyzer.testes;
+import br.com.mailanalyzer.dao.actions.ActionProcessedMessage;
+import br.com.mailanalyzer.domain.ProcessedMessage;
+import br.com.mailanalyzer.domain.DomainObject;
+import br.com.mailanalyzer.domain.Receiver;
+import br.com.mailanalyzer.utils.cripto.Encriptador;
+
+/**
+ *
+ * @author Herlayne
+ * @contact fran.herlayne@gmail.com
+ * @version 1.0
+ * @Date 07-05-2011
+ */
+public class TesteProcessedMessage {
+    public static void main(String[] args) throws InstantiationException, IllegalAccessException {
+        ActionProcessedMessage x= new ActionProcessedMessage();
+        
+        ProcessedMessage myMessage=new ProcessedMessage();
+        
+    }
+
+}
